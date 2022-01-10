3b26e4ab4ecc5f9c1f621821a25b06dfca8c1269
sirawit.mahanin <sirawit.mahanin@accenture.com>
2022-01-10 10:39:26 +0700

    Module3: Finished

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase // Controller
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        //[HttpGet("api/authors")]
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+            //return new JsonResult(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            if (authorsFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(authorsFromRepo);
+        }
+    }
+}
---
+++ CourseLibrary.API/Models/AuthorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CourseLibrary.API.Models
+{
+    public class AuthorDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string MainCategory { get; set; }
+
+    }
+}
